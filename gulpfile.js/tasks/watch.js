40da14d68df5928a4e8ac85054ff99c6559b5150
Charlie <bringking@gmail.com>
2015-09-17 14:34:10 -0700

    fixed icon

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -3,12 +3,10 @@ var html     = require('../config/html');
 var iconFont = require('../config/iconFont');
 var images   = require('../config/images');
 var sass     = require('../config/sass');
-var php     = require('../config/php');
 var fonts    = require('../config/fonts');
 var watch    = require('gulp-watch');
 
 gulp.task('watch', ['browserSync'], function() {
-  watch(php.src, function() { gulp.start('php'); });
   watch(images.src, function() { gulp.start('images'); });
   watch(sass.src, function() { gulp.start('sass'); });
   watch(iconFont.src, function() { gulp.start('iconFont'); });
