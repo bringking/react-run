530d6cb80d826554de81c689e8ef46d4520e98be
Charlie <bringking@gmail.com>
2016-01-10 18:35:59 -0700

    fixed script

--- app/javascripts/constants/index.js
+++ app/javascripts/constants/index.js
@@ -1,10 +1,10 @@
 export const initialScript = `/**
- * Welcome to React Bin! The in-browser React testing environment.
- * This component is required for React Bin to work,
+ * Welcome to React.run! The in-browser React testing environment.
+ * This component is required for React.run to work,
  * and it your main Application entry point. You can feel free
  * to modify it and add other components. Enjoy!
  *
- * Globals Usage: Currently React Bin exposes only two global variables for your use,
+ * Globals Usage: Currently React.run exposes only two global variables for your use,
  * "React" and "ReactDOM". There are plans to add more soon!
  */
 class Main extends React.Component {
@@ -12,6 +12,6 @@ class Main extends React.Component {
         super();
     }
     render() {
-        return (<div>Welcome to React Bin!</div>)
+        return (<div>Welcome to React.run!</div>)
     }
 }`;
