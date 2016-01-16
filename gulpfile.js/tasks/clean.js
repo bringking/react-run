0a97904e7c7c59b6cf7eeddc5a7bbd01da02333f
Charles King <King@Charles-Kings-iMac.local>
2016-01-16 13:59:54 -0700

    remove iconFon

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,12 +2,10 @@ var gulp = require('gulp');
 var del = require('del');
 var config = require('../config');
 var htmlConfig = require('../config/html');
-var iconFontConfig = require('../config/iconFont');
 
-gulp.task('clean', function( cb ) {
+gulp.task('clean', function (cb) {
     return del([
         config.publicAssets,
-        htmlConfig.dest,
-        iconFontConfig.sassDest
+        htmlConfig.dest
     ]);
 });
