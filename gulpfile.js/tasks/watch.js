77ea8f285f0ad8b17115d8a850974e94a56b28d0
Charles King <King@Charles-Kings-iMac.local>
2016-01-17 09:27:10 -0700

    fixed gulp watch task

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var images = require('../config/images');
 var sass = require('../config/sass');
 var fonts = require('../config/fonts');
+var watch = require('gulp-watch');
 
 gulp.task('watch', ['browserSync'], function() {
     watch(images.src, function() {
