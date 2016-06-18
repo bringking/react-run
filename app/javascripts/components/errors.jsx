245270b2b58902b22988393368d51339065c3dd4
Charles King <cking@weedmaps.com>
2016-06-18 13:49:32 -0700

    Closes #1, added loop protection thanks to jsbin loop-protect

--- app/javascripts/components/errors.jsx
+++ app/javascripts/components/errors.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from "react"
+import React, { PropTypes } from "react"
 
 /**
  * The Errors modules is responsible for listening to the socket for
@@ -6,72 +6,89 @@ import React, {PropTypes} from "react"
  */
 class Errors extends React.Component {
 
-    constructor( props ) {
-        super(props);
-        this.state = {
-            initialState: true,
-            errorMessage: null,
-            frameError: null
-        };
-
-    }
-
-    componentDidMount() {
-        //this error is effectively a build time error, meaning
-        // babel failed to transpile the code on the server
-        this.props.socket.on("code error", this.onCodeError);
-        //this event will happen if the code successfully transformed,
-        //thus any previous build error is resolved.
-        this.props.socket.on("code transformed", this.onCodeNormal);
-        //this event will happen if the code successfully transformed with webpack,
-        //thus any previous build error is resolved.
-        this.props.socket.on("webpack transform", this.onCodeNormal);
-
-    }
-
-    componentWillReceiveProps( nextProps ) {
-        if ( nextProps.frameError ) {
-            this.setState({frameError: nextProps.frameError, initialState: false});
-        } else {
-            this.setState({frameError: null});
-        }
-    }
-
-    /**
-     * Event handler for code returning to normal
-     */
-    onCodeNormal = ()=> {
-        this.setState({errorMessage: null});
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialState: true,
+      errorMessage: null,
+      frameError: null
     };
 
-    /**
-     * Event handler for receiving an error from the socket
-     * @param e
-     */
-    onCodeError = ( e ) => {
+  }
 
-        if ( e !== this.state.errorMessage ) {
-            this.setState({errorMessage: e, initialState: false});
-        }
+  componentDidMount() {
+    //this error is effectively a build time error, meaning
+    // babel failed to transpile the code on the server
+    this.props.socket.on("code error", this.onCodeError);
+    //this event will happen if the code successfully transformed,
+    //thus any previous build error is resolved.
+    this.props.socket.on("code transformed", this.onCodeNormal);
+    //this event will happen if the code successfully transformed with webpack,
+    //thus any previous build error is resolved.
+    this.props.socket.on("webpack transform", this.onCodeNormal);
 
-    };
+  }
 
-    render() {
-        //no errors
-        if ( !this.state.errorMessage && !this.state.frameError ) {
-            return <div></div>
-        }
-        //show the error
-        return (
-            <div
-                className="error-message animated fadeIn">{this.state.errorMessage || this.state.frameError}</div>
-        );
+  componentWillReceiveProps(nextProps) {
+
+    let nextState = {};
+    if (nextProps.loopError || nextProps.frameError) {
+      nextState.initialState = false;
     }
+
+    this.setState({ frameError: nextProps.frameError, loopError: nextProps.loopError, ...nextState });
+
+  }
+
+  /**
+   * Event handler for code returning to normal
+   */
+  onCodeNormal = ()=> {
+    this.setState({ errorMessage: null });
+  };
+
+  /**
+   * Event handler for receiving an error from the socket
+   * @param e
+   */
+  onCodeError = (e) => {
+
+    if (e !== this.state.errorMessage) {
+      this.setState({ errorMessage: e, initialState: false });
+    }
+
+  };
+
+  render() {
+    //no errors
+    if (!this.state.errorMessage && !this.state.frameError && !this.state.loopError) {
+      return <div></div>
+    }
+
+    // show the loopError, only if there is no error or frameError
+    if (this.state.loopError && (!this.state.errorMessage && !this.state.frameError)) {
+      return <div
+        className="error-message warning-message animated fadeIn"
+        dangerouslySetInnerHTML={{__html:this.state.loopError}}></div>
+    }
+    //show the error
+    if (this.state.frameError) {
+      return (
+        <div
+          className="error-message animated fadeIn"><h1>Runtime Error:</h1><p>{this.state.frameError}</p></div>
+      );
+    }
+
+    return (
+      <div
+        className="error-message animated fadeIn"><h1>Compile Error:</h1><p>{this.state.errorMessage}</p></div>
+    );
+  }
 }
 
 Errors.propTypes = {
-    onErrorReceived: PropTypes.func,
-    onErrorCleared: PropTypes.func
+  onErrorReceived: PropTypes.func,
+  onErrorCleared: PropTypes.func
 };
 
 export default Errors;
