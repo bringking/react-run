bcb1cbb4a22ed357db4825f240f7ff19ec52a521
Charles King <cking@weedmaps.com>
2016-06-18 13:54:47 -0700

    add back uglify

--- gulpfile.js/config/webpack.js
+++ gulpfile.js/config/webpack.js
@@ -89,13 +89,13 @@ module.exports = function(env) {
         }
       }),
       new webpack.optimize.DedupePlugin()
-      // new webpack.optimize.UglifyJsPlugin({
-      //     compress: {
-      //       unused: true,
-      //       dead_code: true,
-      //       warnings: false
-      //     }
-      //   })
+      new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            unused: true,
+            dead_code: true,
+            warnings: false
+          }
+        })
     )
   }
 
